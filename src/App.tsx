4de1fd608512dec27672e93fd1250c12512fd0e3
Felipe Manieri <felipemanieri@hotmail.com>
2025-07-09 19:05:20 -0300

    Fix syntax error in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -440,29 +440,10 @@ const App = () => {
                     className="w-10 h-10 bg-blue-700 rounded-lg flex items-center justify-center hover:bg-blue-800 transition-colors"
                   >
                   <svg className="w-5 h-5 text-white" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.1
-  )
-}
-  )
-}38.92-
-  )
-}2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z" />
+                    <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z" />
                     </svg>
                   </button>
                 </div>
-              </div>
-            </div>
-            
-            {/* Suporte */}
-            <div>
-              <h3 className="font-bold mb-4">Suporte</h3>
-              <div className="space-y-2 text-gray-400">
-                <p>Precisa de ajuda?</p>
-                <p>Nossa equipe está pronta para atendê-lo</p>
-                <button className="flex items-center px-4 py-2 bg-teal-600 text-white rounded-lg hover:bg-teal-700 transition-colors mt-3">
-                  <Mail className="w-4 h-4 mr-2" />
-                  Abrir Chamado
-                </button>
               </div>
             </div>
           </div>
@@ -536,5 +517,6 @@ const App = () => {
       </button>
     </div>
   );
+};
 
 export default App;
