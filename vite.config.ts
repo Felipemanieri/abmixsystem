5ebd32f865ab12515be2abdb14293641f438904c
Felipe Manieri <felipemanieri@hotmail.com>
2025-07-16 17:54:51 -0300

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,16 @@
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "client", "src"),
-      "@shared": path.resolve(__dirname, "shared"),
-    },
-  },
   root: path.resolve(__dirname, "client"),
   build: {
-    outDir: path.resolve(__dirname, "dist", "public"), // 🔥 GARANTE QUE O HTML VAI PARA /dist/public
+    outDir: path.resolve(__dirname, "dist/public"),
     emptyOutDir: true,
+    rollupOptions: {
+      input: path.resolve(__dirname, "client/index.html"),
+    },
   },
 });
